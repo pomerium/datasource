405bdc37ff9d900974a152696682ea228dabf10d
Caleb Doxsey <caleb@doxsey.net>
2025-05-30 09:27:58 -0600

    fix imports

--- pkg/blob/upload_test.go
+++ pkg/blob/upload_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/pomerium/datasource/internal/blob"
+	"github.com/pomerium/datasource/pkg/blob"
 )
 
 func TestUploadBundle(t *testing.T) {
