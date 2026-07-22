528b33e4b0ff6d4fd86c6ad83d4aeebe212c4987
Caleb Doxsey <cdoxsey@pomerium.com>
2026-07-22 17:52:30 -0600

    feat(github): support using node ids for user and group ids (#483)

--- pkg/directory/github/config.go
+++ pkg/directory/github/config.go
@@ -20,23 +20,24 @@ type config struct {
 	logger              zerolog.Logger
 	personalAccessToken string
 	url                 *url.URL
+	useNodeIDs          bool
 	username            string
 }
 
 // An Option updates the github configuration.
 type Option func(cfg *config)
 
-// WithLogger sets the logger in the config.
-func WithLogger(logger zerolog.Logger) Option {
-	return func(cfg *config) {
-		cfg.logger = logger
-	}
-}
-
 // WithHTTPClient sets the http client option.
 func WithHTTPClient(httpClient *http.Client) Option {
 	return func(cfg *config) {
 		cfg.httpClient = httpClient
+	}
+}
+
+// WithLogger sets the logger in the config.
+func WithLogger(logger zerolog.Logger) Option {
+	return func(cfg *config) {
+		cfg.logger = logger
 	}
 }
 
@@ -54,6 +55,13 @@ func WithURL(u *url.URL) Option {
 	}
 }
 
+// WithUseNodeIDs sets the use-node-id option in the config.
+func WithUseNodeIDs(useNodeIDs bool) Option {
+	return func(cfg *config) {
+		cfg.useNodeIDs = useNodeIDs
+	}
+}
+
 // WithUsername sets the username in the config.
 func WithUsername(username string) Option {
 	return func(cfg *config) {
@@ -65,6 +73,7 @@ func getConfig(options ...Option) *config {
 	cfg := new(config)
 	WithHTTPClient(http.DefaultClient)(cfg)
 	WithLogger(log.Logger)(cfg)
+	WithUseNodeIDs(false)(cfg)
 	WithURL(defaultURL)(cfg)
 	for _, option := range options {
 		option(cfg)
